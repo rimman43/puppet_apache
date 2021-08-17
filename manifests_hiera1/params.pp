9f27b72f9681e364c32dfa6d4380a4bfbae13b2f
Asad <test@test.com>
2021-08-17 14:27:17 -0400

    first commit

---
+++ manifests_hiera1/params.pp
@@ -0,0 +1,17 @@
+# @summary 
+#   For module variables
+#   "$facts['os']['family']" OR "$::osfamily"
+class apache::params {
+  $install_ensure = 'present'
+  case $facts['os']['family'] {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
+#$facts['os']['family']
+#$::osfamily
+
