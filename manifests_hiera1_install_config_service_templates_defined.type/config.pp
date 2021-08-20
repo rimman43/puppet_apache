d3409ff3bdcbb34e4179e68d61d1d4bde1091ddf
Asad <test@test.com>
2021-08-20 12:21:51 -0400

    first commit

---
+++ manifests_hiera1_install_config_service_templates_defined.type/config.pp
@@ -0,0 +1,15 @@
+# @summary
+#   Install the base Apache package
+#
+class puppet_apache::config {
+#  file { 'apache_config':
+#  file { '/etc/httpd/conf/httpd.conf':
+  file { "${puppet_apache::config_path}":
+    ensure  => $puppet_apache::config_ensure,
+#    path    => $puppet_apache::config_path,
+    source  => "puppet:///modules/puppet_apache/${osfamily}.conf",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+}
