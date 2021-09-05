5d48fad034ebc2c7ac88fabf631caed1929be4a2
rimman43 <test@test.com>
2021-09-05 12:04:32 -0400

    update plans

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,4 +12,8 @@ class puppet_apache::config {
     group   => 'root',
     mode    => '0644',
   }
+  @@host { "${hostname}":
+    host_aliases => "$fqdn",
+    ip           => "$ipaddress",
+  }
 }
